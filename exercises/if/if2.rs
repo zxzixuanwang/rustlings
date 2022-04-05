2d4c0615f67c9f3dae6ae310c89ed5f6a3b5440d
zxzixuanwang <1027660827@qq.com>
2022-04-06 00:17:58 +0800

    if2

--- exercises/if/if2.rs
+++ exercises/if/if2.rs
@@ -9,8 +9,10 @@
 pub fn fizz_if_foo(fizzish: &str) -> &str {
     if fizzish == "fizz" {
         "foo"
-    } else {
-        1
+    } else if fizzish == "fuzz"{
+        "bar"
+    }else{
+        "baz"
     }
 }
 
