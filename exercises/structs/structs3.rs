4f57958654ef18107b8ae6c69e787ab7a1391b76
zxzixuanwang <1027660827@qq.com>
2022-04-14 11:30:18 +0800

    structs

--- exercises/structs/structs3.rs
+++ exercises/structs/structs3.rs
@@ -16,7 +16,7 @@ struct Package {
 impl Package {
     fn new(sender_country: String, recipient_country: String, weight_in_grams: i32) -> Package {
         if weight_in_grams <= 0 {
-            // panic statement goes here...
+            panic!("weight_in_grams is invalid");
         } else {
             Package {
                 sender_country,
@@ -26,12 +26,18 @@ impl Package {
         }
     }
 
-    fn is_international(&self) -> ??? {
+    fn is_international(&self) -> bool {
         // Something goes here...
+        if self.sender_country == self.recipient_country{
+            false
+        }else{
+            true
+        }
     }
 
-    fn get_fees(&self, cents_per_gram: i32) -> ??? {
+    fn get_fees(&self, cents_per_gram: i32) -> i32 {
         // Something goes here...
+        cents_per_gram * self.weight_in_grams
     }
 }
 
