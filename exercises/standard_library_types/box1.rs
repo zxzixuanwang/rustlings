a3a97f954343904b5ba548e963db5c3abee47f38
zxzixuanwang <1027660827@qq.com>
2022-05-04 21:42:53 +0800

    box1

--- exercises/standard_library_types/box1.rs
+++ exercises/standard_library_types/box1.rs
@@ -20,9 +20,11 @@
 
 #[derive(PartialEq, Debug)]
 pub enum List {
-    Cons(i32, List),
+    Cons(i32, Box<List>),
     Nil,
 }
+
+use crate::List::{Cons,Nil};
 
 fn main() {
     println!("This is an empty cons list: {:?}", create_empty_list());
@@ -32,13 +34,18 @@ fn main() {
     );
 }
 
+
 pub fn create_empty_list() -> List {
-    unimplemented!()
+    let list  = Nil;
+    list
+//    unimplemented!()
 }
 
 pub fn create_non_empty_list() -> List {
-    unimplemented!()
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    list
 }
+
 
 #[cfg(test)]
 mod tests {
