130a442ca05c7ca38f0bf0fda84fba9fb1398ff1
zxzixuanwang <1027660827@qq.com>
2022-05-04 16:07:51 +0800

    quiz done

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -19,12 +19,13 @@ mod tests {
 
     #[test]
     fn returns_twice_of_positive_numbers() {
-        assert_eq!(times_two(4), ???);
+        assert_eq!(times_two(4), 8);
     }
 
     #[test]
     fn returns_twice_of_negative_numbers() {
         // TODO replace unimplemented!() with an assert for `times_two(-4)`
-        unimplemented!()
+        //unimplemented!()
+        assert_eq!(times_two(-4),-8);
     }
 }
